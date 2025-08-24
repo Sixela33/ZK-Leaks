ccb9427f5560839074cdf4a7c60a4ad40b3f19c0
Steven <stevenmolina2205@gmail.com>
2025-08-24 11:54:24 -0300

    feat: new organized frontend

--- frontend-vite-react/src/lib/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL || 'http://localhost:3000',
-  headers: {
-    'Content-Type': 'multipart/form-data',
-  },
-});
